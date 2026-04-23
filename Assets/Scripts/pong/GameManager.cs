7ba564fa02906c7e0692c6895456bd63649205a7
shota67-git <shota67@taivs.tp.edu.tw>
2026-04-23 13:48:16 +0800

    0423

---
+++ Assets/Scripts/pong/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(-1)]
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Ball ball;
+    [SerializeField] private Paddle playerPaddle;
+    [SerializeField] private Paddle computerPaddle;
+    [SerializeField] private Text playerScoreText;
+    [SerializeField] private Text computerScoreText;
+
+    private int playerScore;
+    private int computerScore;
+    public GameObject ballprefab;
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        SetPlayerScore(0);
+        SetComputerScore(0);
+        NewRound();
+    }
+
+    public void NewRound()
+    {
+        playerPaddle.ResetPosition();
+        computerPaddle.ResetPosition();
+        GameObject[] balls = GameObject.FindGameObjectsWithTag("Ball");
+        foreach (GameObject a in balls) Destroy(a);
+        
+        ball.ResetPosition();
+
+        CancelInvoke();
+        Invoke(nameof(StartRound), 1f);
+    }
+
+    private void StartRound()
+    {
+        ball.AddStartingForce();
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject go = Instantiate(ballprefab);
+            int px = Random.Range(-4, 4);
+            int py = Random.Range(-3, 3);
+            go.transform.position = new Vector3(px, py, 0);
+            Ball ballscript = go.GetComponent<Ball>();
+            ballscript.AddStartingForce();
+        }
+    }
+
+    public void OnPlayerScored()
+    {
+        SetPlayerScore(playerScore + 1);
+        NewRound();
+    }
+
+    public void OnComputerScored()
+    {
+        SetComputerScore(computerScore + 1);
+        NewRound();
+    }
+
+    private void SetPlayerScore(int score)
+    {
+        playerScore = score;
+        playerScoreText.text = score.ToString();
+    }
+
+    private void SetComputerScore(int score)
+    {
+        computerScore = score;
+        computerScoreText.text = score.ToString();
+    }
+
+}
